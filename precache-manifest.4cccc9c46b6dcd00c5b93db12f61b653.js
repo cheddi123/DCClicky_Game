fef51891b1c02527aeb24c9dac54549c71804def
Cheddi Charles <cheddi.charles@gmail.com>
2019-01-19 13:42:47 -0600

    Updates

---
+++ precache-manifest.4cccc9c46b6dcd00c5b93db12f61b653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3382254c1d302faba91f",
+    "url": "/DCClicky_Game/static/js/runtime~main.3382254c.js"
+  },
+  {
+    "revision": "515d16389cd73770265f",
+    "url": "/DCClicky_Game/static/js/main.515d1638.chunk.js"
+  },
+  {
+    "revision": "b4a0c8291ae6662d7ee6",
+    "url": "/DCClicky_Game/static/js/1.b4a0c829.chunk.js"
+  },
+  {
+    "revision": "515d16389cd73770265f",
+    "url": "/DCClicky_Game/static/css/main.ca9142bc.chunk.css"
+  },
+  {
+    "revision": "37bbece4af05c266d28f2e7803b98d4c",
+    "url": "/DCClicky_Game/index.html"
+  }
+];
